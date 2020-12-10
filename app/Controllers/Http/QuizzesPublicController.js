369a3d4a87c95c407f9e96cff33cdd20f1b71009
Magno Vianna <mvianna@live.com>
2020-12-09 23:18:22 -0300

    Rota de listagem de forma publica que traz todas as quizzes, questions e answers

---
+++ app/Controllers/Http/QuizzesPublicController.js
@@ -0,0 +1,20 @@
+'use strict'
+const Quiz = use('App/Models/Quiz')
+
+class AllQuizzController {
+  async index () {
+    const quiz = await Quiz
+      .query()
+      .setHidden(['created_at', 'updated_at', 'user_id'])
+      .with('questions', questionsQuery => {
+        questionsQuery.setHidden(['created_at', 'updated_at', 'quiz_id'])
+          .with('answers', asnwersQuery => {
+            asnwersQuery.setHidden(['created_at', 'updated_at', 'question_id'])
+          })
+      })
+      .fetch()
+    return quiz
+  }
+}
+
+module.exports = AllQuizzController
